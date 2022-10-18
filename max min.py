48f9709ebb6a61d2f2d6453e1474c13e5abc69bd
m4ns0or <101454851+m4ns0or@users.noreply.github.com>
2022-10-18 21:24:54 +0600

    Update max min.py

--- max min.py
+++ max min.py
@@ -1,3 +1,8 @@
+#Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+#Examples (Input -> Output)
+
+
+
 lst1 = [4, 6, 2, 1, 9, 63, -134, 566]
 lst2 = [-52, 56, 30, 29, -54, 0, -110]
 lst3 = [42, 54, 65, 87, 0]
